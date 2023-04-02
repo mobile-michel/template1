0dcb9a2f38beb1497d2f9010877451bb73912b94
Michel Maillard <info@maillard.li>
2023-04-02 17:58:09 +0200

    First commit

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
